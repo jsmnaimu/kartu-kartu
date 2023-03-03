807755e2bfcd1c8808aca5493404b5278bc304be
jasmine.ainal <jasmine.ainal@ui.ac.id>
2023-03-03 17:32:56 +0700

    fix settings.py

--- project_kartu/settings.py
+++ project_kartu/settings.py
@@ -140,5 +140,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['https://kartukartu.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://kartukartu.up.railway.app']
 
